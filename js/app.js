b5021954151f1718439b7202ae1cbe205c058bc4
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-05-25 02:56:23 -0300

    Mejoras en la barra de navegación y en las capturas.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,22 @@ var app = angular.module('app', []);
 app.controller("MainCtrl", function($scope) {
   $scope.data = {};
 
+  $scope.seleccionar_captura = function(numero) {
+    $scope.data.programa.captura = numero;
+  }
+
+  $scope.esta_activa_la_captura = function(numero) {
+    return (numero === $scope.data.programa.captura);
+  }
+
+  $scope.mostrar_siguiente_captura = function() {
+    $scope.data.programa.captura += 1;
+
+    if ($scope.data.programa.captura > 3)
+      $scope.data.programa.captura = 1;
+
+  }
+
   $scope.data.programa = {
     titulo: "Thomas Was Alone",
     nick: 'thomas',
